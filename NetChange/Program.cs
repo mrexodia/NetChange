cb429c24e0ee21e78c8c1c6b563ec772020f8265
mrexodia <mr.exodia.tpodt@gmail.com>
2016-12-05 15:44:01 +0100

    initial commit

---
+++ NetChange/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetChange
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
