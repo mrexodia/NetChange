649df25ab5f84f75e79ac2371b5eef6ada8eede2
Billum <tomas.billum@outlook.com>
2016-12-18 12:19:26 +0100

    not for disjunct networks

---
+++ NetChange/disjunct.bat
@@ -0,0 +1,6 @@
+start bin\Debug\NetChange 1100 1102 1101
+start bin\Debug\NetChange 1101 1100 1102
+start bin\Debug\NetChange 1102 1100 1101
+start bin\Debug\NetChange 1104 1105
+start bin\Debug\NetChange 1105 1104 1106
+start bin\Debug\NetChange 1106 1105
